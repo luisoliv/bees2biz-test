5c449a107d014b7c0e7008bb9e871808f9a24368
luisoliv <developeroliva@gmail.com>
2022-11-01 23:13:37 -0500

    Base create-next-app with Material-UI

---
+++ my-test/pages/aspirants.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+export const Aspirants = () => {
+  return (
+    <div>
+      <Button variant="contained">Hello World</Button>
+    </div>
+  )
+}
+
+export default Aspirants;
